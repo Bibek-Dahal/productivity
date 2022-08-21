816571e7ec0bb2f10347b7ba2864cf95af1a7ac4
Bibek-Dahal <bibekdahal479@gmail.com>
2022-08-21 11:00:00 +0545

    chatting feature added

--- productivity/backend/index.js
+++ productivity/backend/index.js
@@ -5,15 +5,30 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
 import profile from './routes/profile.js'
 import group from './routes/group.js'
-import path from 'path'
+import {Server} from 'socket.io'
+import cors  from "cors";
+import Chat from './models/Chat.js';
 const base_dir = process.cwd()
 export default base_dir
 dotenv.config()
+let users = []
+
+
+//socket io conf
+const io = new Server({
+    cors: {
+        // origin: ["http://127.0.0.1:3000","http://localhost:3000"],
+        origin:"*"
+        // credentials: true
+  }
+})
 
 const app = express()
 // app.use(express.json());
 //parse application/json
 app.use(bodyParser.json())
+
+app.use(cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,8 +41,7 @@ const options = {
 }
 
 connectDb(uri,options)
-// app.set('views', path.join(process.cwd(), 'views'));
-// app.set('view engine', 'ejs');
+
 /*
     middlewares
 */
@@ -37,9 +51,55 @@ app.use('/api/group',group) //handle group routes
 
 
 
-app.listen(port,()=>{
+const server = app.listen(port,()=>{
     console.log(`listening on port:${port}`)
 })
 
+//listen for websocket
+io.listen(server)
+
+io.on('connection',(socket)=>{
+    console.log('new web socket connection added')
+    socket.on('new-user',(data)=>{
+        users.push(socket.id)
+        console.log(users)
+        socket.join(`${data.roomId}`)
+        
+    })
+
+    socket.on('disconnect', () => {
+        
+        let filtered = users.filter(function(value, index, arr){ 
+            return value == socket.id;
+        });
+        users.splice(filtered[0],1)
+        console.log('remaining users',users)
+    });
+
+    socket.on('new-chat-message',async (data)=>{
+        const {room,message,userId} = data
+        try{
+            const chat = await Chat.create({
+                group: room,
+                user: userId,
+                text: message,
+            })
+
+            const populated_chat = await chat.populate('user')
+            console.log(populated_chat)
+        
+
+            // console.log('username',chat.user.username)
+
+            io.in(room).emit("new-chat-message",{user:populated_chat.user.username,message:message})
+            console.log('chat message created')
+        }catch(error){
+            console.log(error)
+            console.log('message cannot be created')
+        }
+        
+    })
+
+});
 
 
