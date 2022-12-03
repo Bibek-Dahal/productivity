9f9fb00feee0363f54572c86717bc17fd1a7f486
ashish-kumar-khatri <ashishkmrkhatri19@gmail.com>
2022-12-03 12:49:00 +0545

    added baseurl env

--- productivity/src/utils/endpoints/baseURL.js
+++ productivity/src/utils/endpoints/baseURL.js
@@ -1,8 +1,9 @@
-const baseURL = 'http://127.0.0.1:8000';
+// const baseURL = 'http://127.0.0.1:8000';
 // const baseURL = 'http://192.168.1.80:8000';
 // export const baseURLFront = 'http://192.168.1.80:3000';
-export const baseURLFront = 'http://127.0.0.1:3000';
-
+// export const baseURLFront = 'http://127.0.0.1:3000';
+const baseURL = process.env.REACT_APP_BASEURL
+export const baseURLFront = process.env.REACT_APP_BASEURL_FRONT
 
 
 export default baseURL;
